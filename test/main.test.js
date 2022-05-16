a37b722b694f499244b9b5890cf43ce576dcdd25
Nadir <nadirkhan98@outlook.com>
2022-05-16 16:29:16 +0100

    test suite optimised with a beforeAll and afterAll

--- test/main.test.js
+++ test/main.test.js
@@ -4,12 +4,17 @@ const server = require("../scripts/server");
 describe("api server", () => {
   let api;
   beforeAll(() => {
-    api = server.listen(5000, () => {
-      console.log("test 5000");
+    api = server.listen(3000, () => {
+      console.log("test 3000");
     });
   });
 
   test("it responds to get / with status 200", (done) => {
     request(api).get("/").expect(200, done);
+  });
+
+  afterAll((done) => {
+    console.log("Gracefully stoppinf test server");
+    api.close(done);
   });
 });
