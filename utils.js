f6ca86ba5589ddcd275ae4b346fb0c6026fa1d90
mabus <m.buscaroli@outlook.com>
2022-05-16 12:12:11 +0100

    can now add a comment

--- utils.js
+++ utils.js
@@ -18,9 +18,6 @@ class Post {
     this.date = this.stringDate
   }
 
-  // getting the number of posts
-  0
-
   get stringDate() {
     // return todayWithDay(this.date)
     return this.date.toString().replace('T', '').replace(/GM.+/i, '').trim()
@@ -46,6 +43,7 @@ class Comment {
     this.link = link
     this.date = new Date()
     this.postRef = postObject.id
+    this.date = this.stringDate
   }
   get stringDate() {
     // return todayWithDay(this.date)
@@ -69,8 +67,8 @@ function readDataFromFile(filename) {
   try {
     const data = fs.readFileSync(filename)
     const jsonData = JSON.parse(data)
-    console.log(`readDataFromFile - logging JSOn string ->\n${data}\n `)
-    console.log('readDataFromFile - logginf JS object -> \n', jsonData)
+    // console.log(`readDataFromFile - logging JSOn string ->\n${data}\n `)
+    // console.log('readDataFromFile - logginf JS object -> \n', jsonData)
     return jsonData
   } catch (err) {
     console.error(err)
@@ -82,7 +80,7 @@ function readDataFromFile(filename) {
 function writePostToFile(filename, data) {
   try {
     const stringData = JSON.stringify(data)
-    console.log('\nwritePostToFile - stringData -> ', stringData)
+    // console.log('\nwritePostToFile - stringData -> ', stringData)
     fs.writeFileSync(filename, stringData)
     // return stringData
   } catch (err) {
@@ -100,7 +98,7 @@ function addPost(post) {
 
   // getting the posts as a JS Object
   let data = readDataFromFile(dataUrl)
-  console.log('\naddPost - data -> ', data)
+  // console.log('\naddPost - data -> ', data)
 
   // const postsNumber = data.length
   // console.log('data length --------', postsNumber)
@@ -109,7 +107,7 @@ function addPost(post) {
   const newPost = new Post(post.title, post.body, post.link)
   // newPost.id = postsNumber + 1
 
-  console.log('newPost -> ', newPost)
+  // console.log('newPost -> ', newPost)
   // adding the new Post to the data Array
   data = [...data, newPost]
   console.log('\naddPost - data after post added -> ', data)
@@ -117,55 +115,6 @@ function addPost(post) {
 
   return updatedData
 }
-
-function createCommentAndAppend(comment) {
-  const newComment = comment
-}
-
-// TESTING
-const firstPost = new Post(
-  'Welcome',
-  'You are the first visitor',
-  'http://thisLink.com'
-)
-// console.log('\nfirstPost getData -> ', firstPost.getData)
-
-const firstComment = new Comment('That post sucked!', firstPost)
-// console.log('\nfirstComment getData -> ', firstComment.getData)
-
-const secondComment = new Comment('Not too bad...', firstPost)
-// console.log('\nsecondComment getData -> ', secondComment.getData)
-
-const thirdComment = new Comment('Not too bad...', firstPost)
-// console.log('\nthirdComment getData -> ', thirdComment.getData)
-
-const secondPost = new Post(
-  'My Post is better',
-  'Definitely better than the first one!',
-  'http://linkToBeValidated.here'
-)
-// console.log('\nsecondPost -> ', secondPost.getData)
-
-const fourthComment = new Comment(
-  'First Comment for the second Post',
-  secondPost
-)
-// console.log('\nfourthComment getData -> ', fourthComment.getData)
-
-const fifthComment = new Comment(
-  'Second Comment for the second Post',
-  secondPost
-)
-// console.log('\nfifthComment getData -> ', fifthComment.getData)
-
-// const firstDoc = new Document(console.log, firstPost)
-// firstDoc.describeType()
-
-console.log('\nwriting and reading to the DB - testing starts here...\n')
-
-console.log('reading data from file\n', readDataFromFile(dataUrl))
-
-console.log('\n----------\n')
 
 // Use this to add a Post
 // const dataAfterOnePostAdded = addPost({
@@ -186,35 +135,37 @@ console.log('\n----------\n')
 //   reactions: { laugh: 0, thumbUp: 0, poo: 0 },
 // })
 
-console.log(
-  'reading data from file after one post added\n',
-  readDataFromFile(dataUrl)
-)
+// console.log(
+//   'reading data from file after one post added\n',
+//   readDataFromFile(dataUrl)
+// )
 
-function readCommentsOfPost(filename) {
-  try {
-    const data = fs.readFileSync(filename)
-    const jsonData = JSON.parse(data)
-    console.log(`readDataFromFile - logging JSOn string ->\n${data}\n `)
-    console.log('readDataFromFile - logginf JS object -> \n', jsonData)
+// function readCommentsOfPost(filename) {
+//   try {
+//     const data = fs.readFileSync(filename)
+//     const jsonData = JSON.parse(data)
+//     console.log(`readDataFromFile - logging JSOn string ->\n${data}\n `)
+//     console.log('readDataFromFile - logginf JS object -> \n', jsonData)
 
-    const postIndex = jsonData.find
-  } catch (err) {
-    console.error(err)
-  }
-}
+//     const postIndex = jsonData.find
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
 
 // Find a post by using its UUID
 function findPostById(post, filename) {
   const allPostsObj = readDataFromFile(filename)
-  console.log('\nfindPostByUUID - allPosts.Obj -> ', allPostsObj)
+  // console.log('\nfindPostByUUID - allPosts.Obj -> ', allPostsObj)
   // const postId = (post) => post.id
   // console.log('\nfindPostByUUID - postId -> ', postId)
+
+  // console.log('\n===== findPostById - allPostsObj type -> ', typeof allPostsObj)
 
   const targetPostIndex = allPostsObj.findIndex(
     (postElement) => postElement.id === post.id
   )
-  console.log('\nfindPostByUUID - targetPostIndex -> ', targetPostIndex)
+  // console.log('\nfindPostByUUID - targetPostIndex -> ', targetPostIndex)
 
   if (targetPostIndex === -1) {
     console.log('Post could not be found')
@@ -226,8 +177,50 @@ function findPostById(post, filename) {
   }
 }
 
+function addComment(post, comment, filename) {
+  const allPostsObj = readDataFromFile(filename)
+  console.log('\naddComment - allPosts.Obj -> ', allPostsObj)
+  console.log('\naddComment - type of allPosts.Obj -> ', typeof allPostsObj)
+
+  const newComment = new Comment(comment.body, post, (link = ''))
+
+  const targetPostIndex = allPostsObj.findIndex(
+    (postElem) => postElem.id === post.id
+  )
+  console.log('***** addComment - targetPostIndex -> ', targetPostIndex)
+  if (targetPostIndex === -1) {
+    return null
+  } else {
+    console.log(
+      '\n\nAAAAA addComment - allPostsObj[targetPostIndex] -> ',
+      allPostsObj[targetPostIndex]
+    )
+    allPostsObj[targetPostIndex].comments.push(newComment)
+
+    writePostToFile(dataUrl, allPostsObj)
+  }
+}
+
+// TESTING
+
+console.log('\nwriting and reading to the DB - testing starts here...\n')
+
+console.log('reading data from file\n', readDataFromFile(dataUrl))
+
+console.log('\n----------\n')
+
 // Sample Post for testing purposes
-const samplePost = {
+// const samplePost = {
+//   id: 'dbdae69a-99d5-4c71-addc-fbc831f13f01',
+//   title: 'Post Three added',
+//   body: 'Post 3 added',
+//   link: 'ccc',
+//   date: 'Mon May 16 2022 09:41:59',
+//   comments: [],
+//   reactions: { laugh: 0, thumbUp: 0, poo: 0 },
+// }
+
+const samplePost2 = {
   id: 'dbdae69a-99d5-4c71-addc-fbc831f13f01',
   title: 'Post Three added',
   body: 'Post 3 added',
@@ -237,4 +230,20 @@ const samplePost = {
   reactions: { laugh: 0, thumbUp: 0, poo: 0 },
 }
 
+const sampleComment1 = {
+  id: 34567,
+  body: 'Great Post, wanted to comment on it',
+  link: 'http://wherever.com',
+  date: 'Mon May 16 2022 17:27:52',
+  postRef: '67197f9a-fb38-4a81-b223-c76e695cd0fa',
+}
+
+// Call this to find a post by ID
 // findPostById(samplePost, dataUrl)
+
+// const comment1 = new Comment('Great Job', samplePost2, 'http://link1.com')
+// console.log('comment1 -> ', comment1)
+
+// Use this when adding comments
+const postAfterCommentAdded = addComment(samplePost2, sampleComment1, dataUrl)
+console.log('Post after comment added', postAfterCommentAdded)
