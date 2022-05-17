ef9efac869d02b5e4fd11828d40daadffd377ab4
Nadir <nadirkhan98@outlook.com>
2022-05-17 13:48:57 +0100

    tried testing two more routes

--- test/main.test.js
+++ test/main.test.js
@@ -21,6 +21,10 @@ describe("api server", () => {
     request(api).get("/posts/ajdj-sds2-sdsd").expect(200, done);
   });
 
+  test("it responds to get /posts/doesnotexist with status 404", (done) => {
+    request(api).get("/posts/doesnotexist").expect(404, done);
+  });
+
   test("it responds to post /posts with status 201", (done) => {
     const testData = {
       title: "test1 title",
@@ -29,6 +33,26 @@ describe("api server", () => {
     };
 
     request(api).post("/posts").send(testData).expect(201, done);
+  });
+
+  test.only("it responds to post /posts/comments with status 201", (done) => {
+    const testData = {
+      comment: "bhsdifsdiffb",
+    };
+
+    request(api).post("/posts/comments").send(testData).expect(201, done);
+  });
+
+  test.only("it responds to post /posts/emojis with status 201", (done) => {
+    const testData = {
+      emoji: " ",
+    };
+
+    request(api).post("/posts/emojis").send(testData).expect(201, done);
+  });
+
+  test("it responds to delete /posts with status 404", (done) => {
+    request(api).delete("/posts").expect(404, done);
   });
 
   afterAll((done) => {
